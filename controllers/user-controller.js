a3d440e660895bc952bc608d868d0717e6778f9a
Franck DAKIA <dakiafranck@gmail.com>
2019-07-24 11:50:01 +0000

    update default user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,20 @@
 class UserController extends BaseController {
+	/**
+	 * Show the get started message
+	 * 
+	 * @param {Object} res
+	 * @param {Object} req
+	 * @return {void}
+	 */
 	index(req, res) {
-		res.send("Hello world");
+		res.render("app");
 	}
 }
+
+// Optimise class loader
+let instance;
+if (!typeof instance instanceof UserController) {
+	instance = new UserController;
+}
+
+module.exports = instance;
