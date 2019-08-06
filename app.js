4b00dd784a76a70df2c757dc3490f618102459d2
Franck DAKIA <dakiafranck@gmail.com>
2019-08-06 06:18:52 +0000

    add request parser

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ global.__config = require('config');
 const glob = require("glob");
 const app = require('express')();
 
+// We load the request parse
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Set the view engine
 app.set("view engine", __config.view.engine);
 app.set("views", __config.view.directory);
+
+app.use();
 
 // Load application route
 glob("./routes/*.js", function (er, files) {
