388f97a637bad010e6235170d484916d21f3cfc5
Franck DAKIA <dakiafranck@gmail.com>
2019-08-06 13:29:02 +0000

    add session configuration

--- config/default.js
+++ config/default.js
@@ -1,11 +1,16 @@
 const auth = require("./auth");
 const view = require("./view");
 const certificate = require("./certificate");
+const session = require("./session");
 
-// Base application configuration
-module.exports = {
+// Prevent for update
+const config = {
   auth: auth,
   port: process.env.APP_PORT,
   view: view,
-  certificate: certificate
+  certificate: certificate,
+  session: session
 };
+
+// Base application configuration
+module.exports = config;
---
+++ config/session.js
@@ -0,0 +1,13 @@
+// See: https://www.npmjs.com/package/express-session
+module.exports = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    domain: process.env.SESSION_DOMAIN,
+    secure: process.env.SESSION_SECURE,
+    maxAge: process.env.SESSION_DURATION,
+    path: "/",
+    httpOnly: process.env.SESSION_HTTP_ONLY
+  }
+}
