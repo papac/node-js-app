c6b643a234e3cfc32e5beb1cb941d8fbab0a114c
Franck DAKIA <dakiafranck@gmail.com>
2019-10-12 10:05:47 +0000

    fix typo

--- frontend/js/Component/Example.jsx
+++ frontend/js/Component/Example.jsx
@@ -4,7 +4,7 @@ class Example extends React.Component {
   render() {
     return (
       <div>
-        <h1>Welcome to you app</h1>
+        <h1>Welcome to your app</h1>
         <p>This is the base nodejs application structure</p>
         <p>
           <a href="https://github.com/papac/node-js-app" target="_blank">https://github.com/papac/node-js-app</a>
